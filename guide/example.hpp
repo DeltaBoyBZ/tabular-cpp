aad326ef6c6e8b7dfdd62a18973aba44cfff28c1
Matthew Smith <matthew.peter.smith.599@gmail.com>
2022-11-12 17:48:01 +0000

    Initial commit. Migrated all web files to this dedicated branch.

---
+++ guide/example.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include<iostream>
+#include<tabc/tabcpp.hpp>
+
+typedef enum 
+{
+    COLOUR_RED, COLOUR_GREEN, COLOUR_BLUE,
+    COLOUR_YELLOW, COLOUR_PINK, COLOUR_PURPLE,
+    COLOUR_ORANGE, COLOUR_OTHER
+} Colour;
+
+//TABLE_BEGIN People
+//TABLE_END People
+
